c9a1d9db42af8e35f612b16d1e9c267527eba38b
er00aman <amandaviet2021@gmail.com>
2025-08-20 15:33:19 +0530

    add api in whiteboard and live session

--- app/routes/indexRouter.js
+++ app/routes/indexRouter.js
@@ -5,10 +5,12 @@ import authenticationRoutes from "./Authentication/authentication.routes.js";
 import whiteBoardRoutes from "./whiteBoard/whiteBoard.routes.js";
 import liveSessionRoutes from "./LiveSession/liveSession.routes.js";
 import liveSessionParticipantRoutes from "./liveSessionParticipant/liveSessionParticipant.routes.js";
+import livSessionWhiteBoardRoutes from "./liveSessionWhiteBoard/liveSessionWhiteBoard.routes.js";
 
 router.use("/auth", authenticationRoutes);
 router.use("/whiteboard", whiteBoardRoutes);
 router.use("/liveSession", liveSessionRoutes);
 router.use("/liveSessionParticipant", liveSessionParticipantRoutes);
+router.use("/liveSessionWhiteboard", livSessionWhiteBoardRoutes);
 
 export default router;
