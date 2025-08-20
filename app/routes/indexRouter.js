b43ed62a903716543280a1be4ecc735aa9918207
er00aman <amandaviet2021@gmail.com>
2025-08-20 16:31:18 +0530

    update file name live session

--- app/routes/indexRouter.js
+++ app/routes/indexRouter.js
@@ -3,7 +3,7 @@ const router = Router();
 
 import authenticationRoutes from "./Authentication/authentication.routes.js";
 import whiteBoardRoutes from "./whiteBoard/whiteBoard.routes.js";
-import liveSessionRoutes from "../routes/liveSession/liveSession.routes.js";
+import liveSessionRoutes from "./liveSession/liveSession.routes.js";
 import liveSessionParticipantRoutes from "./liveSessionParticipant/liveSessionParticipant.routes.js";
 import livSessionWhiteBoardRoutes from "./liveSessionWhiteBoard/liveSessionWhiteBoard.routes.js";
 
