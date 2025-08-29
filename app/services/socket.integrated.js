aac81fc8879b2a37a9c4c3bf2d46eed5f8e77e50
er00aman <amandaviet2021@gmail.com>
2025-08-29 18:31:31 +0530

    fasfasfas

--- app/services/socket.integrated.js
+++ app/services/socket.integrated.js
@@ -5,7 +5,7 @@ import liveSession from "../model/liveSessions/liveeSession.model.js";
 import liveSessionParticipant from "../model/liveSessionParticipant/liveSessionParticipant.model.js";
 import whiteboardModel from "../model/whiteBoards/whiteBoard.model.js";
 import { ROLE_MAP } from "../constant/role.js";
-
+import crypto from "crypto";
 // ======= Global Variables =======
 let io;
 const roomState = new Map(); // key: sessionId (string) -> { whiteboardId, createdBy, streamerSocketId, viewers: Set, sockets: Map, pendingOps, flushTimer }
