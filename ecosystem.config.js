04480c459e669a0261e3c58ce72226f5c4c17144
er00aman <amandaviet2021@gmail.com>
2025-09-10 16:31:14 +0530

    radhe radhe radhe

---
+++ ecosystem.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  apps: [
+    {
+      name: "simplertrading",
+      script: "app.js",
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      env: {
+        NODE_ENV: "development",
+        PORT: 9090,
+      },
+      env_production: {
+        NODE_ENV: "production",
+        PORT: 9090,
+      },
+    },
+  ],
+};
