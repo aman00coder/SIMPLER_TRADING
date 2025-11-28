283ca64144f08c6b1cd55591765109d99f8d34ae
er00aman <amandaviet2021@gmail.com>
2025-11-28 16:21:27 +0530

    radha radha

--- app/controller/liveSession/liveSession.controller.js
+++ app/controller/liveSession/liveSession.controller.js
@@ -3,7 +3,6 @@ import HttpStatus from "http-status-codes";
 import { v4 as uuidv4 } from "uuid";
 import liveSessionModel from "../../model/liveSessions/liveeSession.model.js";
 import whiteBoardModel from "../../model/whiteBoards/whiteBoard.model.js";
-import * as commonServices from "../../services/common.js";
 import { sendSuccessResponse, sendErrorResponse } from "../../responses/responses.js";
 import { errorEn, successEn } from "../../responses/message.js";
 import { getIO } from "../../services/socket.integrated.js"; 
